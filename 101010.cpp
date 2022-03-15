659cd8a3111d99e29eb4c93ecda03b26b62eb1ed
hirbodprime <hirbodprime@gmail.com>
2022-03-15 13:59:49 +0330

    simple codes

---
+++ 101010.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{   
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout << 0;
+        for (size_t j = 1; j < 10000; j++)
+        {
+            cout << j << endl;
+        }
+         
+    }
+    
+
+}
